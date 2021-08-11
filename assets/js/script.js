7fc710348093f902e72cf4db02fda71879f213b7
jackcrosbie <jack.crosbie@mycit.ie>
2021-08-11 16:32:04 +0000

    add js for play button and answer buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,8 @@ let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
+// Quiz Questions
+
 let questions = [
     {
         question: "what is 2 + 2?",
@@ -53,5 +55,62 @@ function startGame () {
     questionCounter = 0;
     score = 0;
     availableQuestions = [...questions];
-    getNewQuestions();
+    getNewQuestion();
 }
+
+function getNewQuestion() {
+    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score);
+
+        return window.location.assign('/end.html');
+    }
+
+    questionCounter++;
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`;
+    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
+
+    let questionsIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionsIndex];
+    questions.innerText = currentQuestion.question;
+
+    choices.forEach(choice => {
+        let number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];
+    })
+
+    availableQuestions.splice(questionsIndex, 1);
+
+    acceptingAnswers = true;
+}
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if(!acceptingAnswers) return;
+
+        acceptingAnswers = false;
+        let selectedChoice = e.target;
+        let selectedAnswer = selectedChoice.dataset('number');
+
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct': 'incorrect';
+
+        /* points added if answer correct */
+        if(classToApply === 'correct') {
+            incremementScore(SCORE_POINTS);
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply);
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestion()
+        }, 1000)
+
+    })
+})
+
+incremementScore = num => {
+    score +=num;
+    scoreText.innerText = score;
+}
+
+startGame ()
